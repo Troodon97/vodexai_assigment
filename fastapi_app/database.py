e2ce4faf1698539ab34e4ac447e0ea2cafab8238
Anand <ballidawanand.gmail.com>
2024-10-13 22:49:21 +0530

    mongoatlas connected 2

--- fastapi_app/database.py
+++ fastapi_app/database.py
@@ -1,12 +1,35 @@
-# database.py
-from pymongo import MongoClient
+# # database.py
+# from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://anand:Troodonmg123@cluster0.kkt3f.mongodb.net/")
+# client = MongoClient("mongodb+srv://anand:Troodonmg123@cluster0.kkt3f.mongodb.net/")
+# db = client["fastapi_db"]
+
+
+# items_collection = db["items"]
+# clockin_collection = db["user_clockin_records"]
+
+
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://anand:Troodonmg123@cluster0.kkt3f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
 db = client["fastapi_db"]
 
 
 items_collection = db["items"]
 clockin_collection = db["user_clockin_records"]
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 
 
